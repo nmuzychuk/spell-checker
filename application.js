d102012dc37f8ca93734512ede6c312faad3a407
Nazarii Muzychuk <muzychuk.n@gmail.com>
2018-08-23 20:14:23 +0300

    Code style fixes

--- application.js
+++ application.js
@@ -1,30 +1,30 @@
-var change = function (event) {
-    check(event.target.value);
-};
+var dict;
 
 var check = function (target) {
     var words = target.split(/\s+/);
     for (var i = 0; i < words.length; i++) {
-        var sanitizedWord = words[i].toLocaleLowerCase().replace(/[^a-z]/gi, '');
+        var sanitizedWord = words[i].toLocaleLowerCase().replace(/[^a-z]/gi, "");
         if (sanitizedWord === "") {
             continue;
         }
         if (dict[sanitizedWord] !== 1) {
-            words[i] = '<span class="not-found">' + words[i] + '</span>';
+            words[i] = "<span class=\"not-found\">" + words[i] + "</span>";
         }
     }
     var resultElement = document.getElementById("result");
     resultElement.innerHTML = words.join(" ");
 };
+var change = function (event) {
+    check(event.target.value);
+};
 
 var textElement = document.getElementById("text");
-textElement.addEventListener('change', change);
-textElement.addEventListener('keyup', change);
+textElement.addEventListener("change", change);
+textElement.addEventListener("keyup", change);
 
-var dict;
 var req = new XMLHttpRequest();
 req.overrideMimeType("application/json");
-req.open('GET', 'https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json', true);
+req.open("GET", "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json", true);
 req.onload = function () {
     dict = JSON.parse(req.responseText);
     check(document.getElementById("text").value);
